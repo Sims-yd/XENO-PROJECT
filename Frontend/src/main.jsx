29730fb85124db368a31a6c2399bc5be8c8d93d6
Md Ammar <ammaratif559@gmail.com>
2025-09-15 22:41:47 +0530

    removed strict mode

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react"
+// import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { GoogleOAuthProvider } from "@react-oauth/google"
 import "./index.css"
@@ -7,10 +7,8 @@ import App from "./App.jsx"
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID // ✅ use import.meta.env in Vite
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <App />
     </GoogleOAuthProvider>
-  </StrictMode>
 )
 

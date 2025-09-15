caafd2a5c6bbb33df8a1ecdb73b51d1b688a2652
Md Ammar <ammaratif559@gmail.com>
2025-09-15 22:51:54 +0530

    fixed cors again

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://xeno-project-production.up.railway.app',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
